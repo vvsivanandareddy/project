748ee331bf2517e822e1104a03cf22b3dc3c2d22
vv sivananda reddy <v.v.sivanandareddy@gmail.com>
2019-10-27 20:49:04 +0530

    added name in app.java

--- MavenHelloWorldProject/src/main/java/com/jcg/maven/App.java
+++ MavenHelloWorldProject/src/main/java/com/jcg/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! hi there how are you" );
+        System.out.println( "Hello World! hi there how are you i am vv sivananda reddy" );
     }
 }
