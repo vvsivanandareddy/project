62605fb15ef487ad94ce1e326fdf56727edc9bb9
sivananda reddy vv <v.v.sivanandareddy@gmail.com>
2019-11-18 12:10:21 +0530

    modified app.java

--- MavenHelloWorldProject/src/main/java/com/jcg/maven/App.java
+++ MavenHelloWorldProject/src/main/java/com/jcg/maven/App.java
@@ -1,14 +1,8 @@
 package com.jcg.maven;
-
-/**
- * Hello world!!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! hi there how are you i am vv sivananda reddy" );
-        System.out.println( "Hello World! hi theresdsdfgsgfsgfsfsf " );
+        System.out.println( "hi this is siva" );
     }
 }
